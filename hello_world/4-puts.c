1dc108699dd6dcafb3298c6e72e48c19dc502149
chris85gillis <chris85gillis@gmail.com>
2023-05-24 12:20:32 -0700

    C-Hello World

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /*
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
+ * main - Entry point
+ *
+ * Description: A program that prints a line with puts function
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
