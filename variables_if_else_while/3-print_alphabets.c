c33ae6254e9cc184defaf149880309afd8abcaaf
chris85gillis <chris85gillis@gmail.com>
2023-05-25 12:58:41 -0700

    C - Variables, if, else, while

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,13 +3,14 @@
  * main - Entry point
  *
  * Return: Always 0 (success)
- */ 
+ */
 
 
 int main(void)
 {
 	int lowerCase = 'a';
 	int upperCase = 'A';
+
 	while (lowerCase <= 'z')
 	{
 		putchar(lowerCase);
