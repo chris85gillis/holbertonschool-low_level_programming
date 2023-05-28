19419f55ad14e7f26c68273a2193739acb2fe64c
chris85gillis <chris85gillis@gmail.com>
2023-05-28 13:54:19 -0700

    C - Functions, nested loops

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: the number to be treated
- * return: value of the last digit of number
+ * Return: value of the last digit of number
  */
 
 int print_last_digit(int n)
