cb355ab7c99e72e401788b5085617ee5863af84c
chris85gillis <chris85gillis@gmail.com>
2023-05-31 11:45:07 -0700

    C - More functions, more nested loops

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -15,19 +15,15 @@
 void print_numbers(void)
 
 {
+	char c;
 
-		char c;
+	for (c = '0'; c <= '9'; c++)
 
+	{
+		putchar(c);
 
+	}
 
-			for (c = '0'; c <= '9'; c++)
-
-					{
-
-								putchar(c);
-
-									}
-
-				putchar('\n');
+	putchar('\n');
 
 }
