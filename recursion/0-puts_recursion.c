e3e5a971ae87ee2fbc66b486ca859415e17c4259
chris85gillis <chris85gillis@gmail.com>
2023-06-14 16:26:16 -0700

    C - Recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,13 +3,13 @@
 /**
  * _puts_recursion - function like puts();
  * @s: input
- * 
+ *
  * Return: Always 0 (success)
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 	_putchar(*s);
 	_puts_recursion(s + 1);
