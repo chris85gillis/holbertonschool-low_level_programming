3dbc2f2afd15fc6ebadc8f04620982b272e25c58
chris85gillis <chris85gillis@gmail.com>
2023-06-30 13:53:03 -0700

    C - Function pointers

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -10,19 +10,19 @@
 *
 * Description: If separator is NULL, it is not printed.
 * If one of the strings if NULL, (nil) is printed instead.
-* */
+*/
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
 	char *str;
 	unsigned int index;
-	
+
 	va_start(strings, n);
 	for (index = 0; index < n; index++)
 	{
 		str = va_arg(strings, char *);
-		
+
 		if (str == NULL)
 			printf("(nil)");
 		else
