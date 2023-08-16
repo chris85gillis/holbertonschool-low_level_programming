af719ab939683027511ef392250104823ae9c355
chris85gillis <chris85gillis@gmail.com>
2023-08-16 09:17:18 -0700

    fixed gcc issues

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,10 +3,11 @@
 
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
+	
 	if (array == NULL)
 		return -1;  /* Return -1 if array is NULL */
 
-	size_t i;
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = %d\n", i, array[i]);
