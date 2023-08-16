b850734a956a3a5bdbdb3f2376b98b3079b4eb8a
chris85gillis <chris85gillis@gmail.com>
2023-08-16 09:39:18 -0700

    fixed betty coding style issues

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,16 +4,16 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
-	
+
 	if (array == NULL)
-		return -1;  /* Return -1 if array is NULL */
+		return (-1);  /* Return -1 if array is NULL */
 
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return i;  /* Return the index if value is found */
+			return (i);  /* Return the index if value is found */
 	}
 
-	return -1;  /* Return -1 if value is not found in the array */
+	return (-1);  /* Return -1 if value is not found in the array */
 }
