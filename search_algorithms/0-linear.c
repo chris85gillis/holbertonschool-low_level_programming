dc14163b9ccb9518f881021c1a78f272cc107a60
chris85gillis <chris85gillis@gmail.com>
2023-08-16 09:10:47 -0700

    wrote function without a for loop

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,14 +4,15 @@
 int linear_search(int *array, size_t size, int value)
 {
 	if (array == NULL)
-	return -1;  // Return -1 if array is NULL
-    
-	for (size_t i = 0; i < size; i++)
+		return -1;  /* Return -1 if array is NULL */
+
+	size_t i;
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = %d\n", i, array[i]);
 		if (array[i] == value)
-			return i;  // Return the index if value is found
+			return i;  /* Return the index if value is found */
 	}
 
-	return -1;  // Return -1 if value is not found in the array
+	return -1;  /* Return -1 if value is not found in the array */
 }
